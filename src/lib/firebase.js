0b95267bfc7e56a79a39922bdb10510f1d91231b
Raj Mogare <124992967+RajMogare@users.noreply.github.com>
2024-07-08 09:48:17 +0530

    Update firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -6,12 +6,12 @@ import { getStorage } from "firebase/storage";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCQ0t1MPjvF2P_XyLfMXkMb-YtAxAT4enc",
-  authDomain: "reactchatapp-cdd9c.firebaseapp.com",
-  projectId: "reactchatapp-cdd9c",
-  storageBucket: "reactchatapp-cdd9c.appspot.com",
-  messagingSenderId: "1029255301945",
-  appId: "1:1029255301945:web:63bda504f5d682c2a736e8"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 export const app = initializeApp(firebaseConfig);
